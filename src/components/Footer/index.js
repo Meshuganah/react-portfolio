4b68350fd5661168881ff33e90f1d42718c32650
Josh Klein <kleinjjosh74@gmail.com>
2022-07-09 23:12:32 -0400

    pushed footer to bottom of page, added dark background for readability

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <div>
+        <div className='fixed-bottom text-light bg-dark text-center'>
             Email: <a href="mailto:kleinjjosh74@gmail.com">Kleinjjosh74@gmail.com</a>
             LinkedIn: <a href="https://linkedin.com/in/josh-klein-0b0509230" target="_blank" rel='noreferrer'> Josh Klein </a>
             GitHub: <a href="https://github.com/Meshuganah" target="_blank" rel='noreferrer'> Meshuganah </a>
